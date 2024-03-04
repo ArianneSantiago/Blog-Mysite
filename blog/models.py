2f790061ff2ecc5806ee6998ccf17058eac7f56e
Arianne Santiago <ariannesantiago05@gmail.com>
2024-03-04 21:15:38 +0000

    Add single.html

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -23,6 +24,8 @@ class Post(models.Model):
     objects = models.Manager()
     newmanager = NewManager()
 
+    def get_absolute_url(self):
+        return reverse('blog:post_single', args=[self.slug])
 
     class Meta:
         ordering = ('-publish',)
