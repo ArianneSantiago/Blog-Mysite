9c957fabb6be5610d24ab0cf19f3a8db03fb4e0b
Arianne Santiago <ariannesantiago05@gmail.com>
2024-03-05 12:30:56 +0000

    Add feature comment section

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     publish = models.DateTimeField(auto_now_add=True) 
     author = models.ForeignKey (User, on_delete=models.CASCADE, related_name='blog_post')
@@ -32,3 +33,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    content = models.TextField()
+    publish = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("publish",)
+
+    def __str__(self):
+        return f"Comment by {self.name}"
